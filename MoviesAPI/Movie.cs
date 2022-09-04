52848fd6cd02cf250dd90743f59209520d2c7b4e
levanufriev02 <levanufriev02@gmail.com>
2022-09-04 19:08:33 +0300

    Add model

---
+++ MoviesAPI/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace MoviesAPI
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Year { get; set; }
+
+        public string Director { get; set; }
+    }
+}
